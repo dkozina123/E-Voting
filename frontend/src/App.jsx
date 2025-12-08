5c3fecac060a57ed0a5a5179b871a38cc02ced7d
mhrkac01 <mhrkac01@fesb.hr>
2025-12-08 20:46:31 +0100

    ReactRouter dodavanje

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,13 @@ import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-    </div>
+    <BrowserRouter>
+      {
+        <div className="App">
+          <NavBar />
+        </div>
+      }
+    </BrowserRouter>
   );
 }
 
