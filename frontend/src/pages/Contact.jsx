bdb5da5ef323638681e55cb923b3d9ee9295603d
KarloKomsic <karlo.komsic1@proton.me>
2025-12-13 16:27:13 +0100

    Uređena Kontakt stranica

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,9 +1,19 @@
+import '../assets/styles/Contact.css';
 function Contact() {
-    return (
-        <>
-            <h1>Kontakt</h1>
-        </>
-    );
+	return (
+		<>
+			<h1>Kontakt</h1>
+			<span className="contact-span">
+				<div>
+					<p className="contact-p">U slučaju tehničkih poteškoća, ili korisničkih upita, možete nam se javiti putem sljedećih kontakata:<br /><br />
+						E-mail: pomoc@e-voting.gov.hr<br />
+						Telefon (besplatno): <strong>072 200 027</strong> (pozivanje iz inozemstva: <strong>+385 72 200 027)</strong><br />
+						Fax: <strong>+61 412 345 67</strong>
+					</p>
+				</div>
+			</span>
+		</>
+	);
 }
 
 export default Contact;
